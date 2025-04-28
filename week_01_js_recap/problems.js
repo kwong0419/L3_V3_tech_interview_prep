f678671defed441a1ad1df2a2c4ee35697dd43f8
Kevin Wong <kevin.wong954@gmail.com>
2025-04-28 01:12:48 -0400

    complete week_01_js_recap warmup problems

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -1,16 +1,30 @@
 // Reverse a string
 function reverseString(str) {
-
+  let reversed = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i]
+  }
+  return reversed
 }
-  
+
 // Find max in array
 function findMax(arr) {
-
+  let max = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+  }
+  return max
 }
 
 // Check if palindrome
 function isPalindrome(word) {
-    
+  let reversed = ''
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversed += word[i]
+  }
+  return reversed === word
 }
 
-module.exports = { reverseString, findMax, isPalindrome };
+module.exports = {reverseString, findMax, isPalindrome}
